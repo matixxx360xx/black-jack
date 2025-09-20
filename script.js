09eaa9431f11ac31a9a30e446cb04230d83ce722
MSI\mati <matimichalski01@gmail.com>
2025-09-20 14:46:26 +0200

    dodałem funkcję rozdajkarty() do rozdawania kart graczowi i krupierowi + globalny obiekt karty jako instancja klasy Karty, karty wyświetlają się na stronie

--- script.js
+++ script.js
@@ -1,25 +1,74 @@
-const karty = ["♠", "♣", "♦", "♥"];
-const wartosci = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+class Karty{
 
-let talia = [];
-let random = 0;
-function card(){
+    constructor(){
+        this.talia = [];
+        this.karta = 0;
+        this.mojeKarty = "";
+        this.krupierKarty = ""
 
-    for(let i = 0; i < karty.length; i++){
-        for(let j = 0; j < wartosci.length; j++){
-            talia.push(`${karty[i]}-${wartosci[j]}`)
+    }
+
+
+    generujTalie(){
+        const karty = ["♠", "♣", "♦", "♥"];
+        const wartosci = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+        for(let i = 0; i < karty.length; i++){
+            for(let j = 0; j < wartosci.length; j++){
+                this.talia.push(`${karty[i]}-${wartosci[j]}`)
         }
     }
-    
-    for(let i = 0; i < talia.length; i++){
+    }
+
+    tasowanieKart(){
+        let random = 0;
+    for(let i = 0; i < this.talia.length; i++){
          random = Math.floor(Math.random() * (i + 1));
 
-         let temp = talia[i]
-         talia[i] = talia[random]
-         talia[random] = temp;
+         let temp = this.talia[i]
+         this.talia[i] = this.talia[random]
+         this.talia[random] = temp;
     }
-console.log(talia)
-console.log(talia.length)
+    }
+      wypiszTalie() {
+        console.log(this.talia);
+        console.log("Liczba kart:", this.talia.length);
+        return this.talia;
+    }
+
+  
+    rozdajkarty(){
+        for(let i = 0; i < 2; i++){
+            this.mojeKarty+= this.talia[i * 2]; 
+            this.krupierKarty+= this.talia[i * 2 + 1]
+        } 
+        return{
+        mojeKarty: this.mojeKarty,
+        krupierKarty: this.krupierKarty
+        };
+    }
 }
 
-card();
+    const karty = new Karty()
+    function card(){
+            karty.generujTalie();        
+            karty.tasowanieKart(); 
+            document.getElementById("karty").innerHTML = karty.wypiszTalie(); 
+    }
+
+    function rozdajkarty(){
+        const rozdaneKarty = karty.rozdajkarty();
+        document.getElementById("mojekarty").innerHTML = "Moje Karty: " + rozdaneKarty.mojeKarty;
+        document.getElementById("krupierkarty").innerHTML = "Karty Krupiera: " + rozdaneKarty.krupierKarty;
+    }
+
+
+
+
+
+
+
+
+window.onload = function() {
+    card();
+    rozdajkarty();
+};
